cb42de8f98fa175438ec97d3b3ef7c18fade0785
Meghdut Mandal <meghdut.mandal@autodesk.com>
2023-02-26 20:54:26 +0530

    changed stuff

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import database_init
 
 conn, current_conn = database_init.get_connection()
 
+
 def cleardb():
     conn.execute("DELETE FROM movieReview")
     conn.commit()
     pass
 
-def insert_into_db(movie_review,Prediction):
-    current_conn.execute("INSERT INTO movieReview (Review, Prediction) VALUES (?, ?)",(movie_review,Prediction))
+
+def insert_into_db(movie_review, Prediction):
+    current_conn.execute("INSERT INTO movieReview (Review, Prediction) VALUES (?, ?)", (movie_review, Prediction))
     conn.commit()
     id = conn.execute('SELECT last_insert_rowid()').fetchall()[0]
     return id
@@ -19,7 +21,7 @@ def insert_into_db(movie_review,Prediction):
 def review(review):
     if review is None or review == "":
         return "Try again"
-    with open("model.pickle",'rb') as f:
+    with open("model.pickle", 'rb') as f:
         pkl = pickle._Unpickler(f)
         pkl.encoding = 'latin1'
         model = pkl.load()
@@ -31,37 +33,41 @@ def review(review):
         return "Positive 😀"
 
 
-
-
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/index.html")
 def index():
     return render_template('index.html')
 
-@app.route('/results.html',methods = ['POST', 'GET'])
+
+@app.route('/results.html', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         print(request.form['result'])
         prediction = review(request.form['result'])
-        id = insert_into_db(request.form['result'],prediction)
+        id = insert_into_db(request.form['result'], prediction)
         id = id[0]
-    
+
         return render_template('results.html', value=prediction, id=id)
+
 
 @app.route('/data.html')
 def data():
     movieReview = conn.execute('SELECT * FROM movieReview').fetchall()
     return render_template('data.html', posts=movieReview)
 
+
 @app.route('/thanks.html')
 def thanks():
     print(100)
     if request.method == 'POST':
-        current_conn.execute("UPDATE movieReview SET Userfeedback = ? WHERE ID = ?",(request.form['correct'], request.form['ID']))
-        conn.commit()  
+        current_conn.execute("UPDATE movieReview SET Userfeedback = ? WHERE ID = ?",
+                             (request.form['correct'], request.form['ID']))
+        conn.commit()
     return render_template('thanks.html')
-    
-if __name__ == "_main_":
+
+
+if __name__ == '__main__':
     app.run(debug=True)
